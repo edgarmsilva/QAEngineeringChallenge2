4a4813bd7ac7e1de8ae28252c8fe69dd5e8d40f6
edgarmsilva <edgar.msilva@gmail.com>
2024-03-06 15:07:03 -0300

    Update regression.test.ts

--- backend/__tests__/regression.test.ts
+++ backend/__tests__/regression.test.ts
@@ -8,51 +8,51 @@ import {
 } from '../../native-app/data/types';
 
 describe('Machine Health Regression tests', () => {
-  // it('calculates WeldingRobot NormalRage', () => {
-  //   const machineName: MachineType = MachineType.WeldingRobot;
-  //   const parts = [
-  //     {name: WeldingRobotPart.ErrorRate, value: 1.0},
-  //     {name: WeldingRobotPart.VibrationLevel, value: 5.0},
-  //     {name: WeldingRobotPart.ElectrodeWear, value: 1.0},
-  //     {name: WeldingRobotPart.ShieldingPressure, value: 15.0},
-  //     {name: WeldingRobotPart.WireFeedRate, value: 10.0},
-  //     {name: WeldingRobotPart.ArcStability, value: 95.0},
-  //     {name: WeldingRobotPart.SeamWidth, value: 2.0},
-  //     {name: WeldingRobotPart.CoolingEfficiency, value: 90.0},
-  //   ];
-  //   const expectedHealth = 100;
+  it('calculates WeldingRobot NormalRage', () => {
+    const machineName: MachineType = MachineType.WeldingRobot;
+    const parts = [
+      {name: WeldingRobotPart.ErrorRate, value: 1.0},
+      {name: WeldingRobotPart.VibrationLevel, value: 5.0},
+      {name: WeldingRobotPart.ElectrodeWear, value: 1.0},
+      {name: WeldingRobotPart.ShieldingPressure, value: 15.0},
+      {name: WeldingRobotPart.WireFeedRate, value: 10.0},
+      {name: WeldingRobotPart.ArcStability, value: 95.0},
+      {name: WeldingRobotPart.SeamWidth, value: 2.0},
+      {name: WeldingRobotPart.CoolingEfficiency, value: 90.0},
+    ];
+    const expectedHealth = 100;
 
-  //   const result = calculateMachineHealth(machineName, parts);
-  //   expect(result).toBe(expectedHealth);
-  // });
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
 
-  // it('calculates PaintingStation NormalRage', () => {
-  //   const machineName: MachineType = MachineType.PaintingStation;
-  //   const parts = [
-  //     {name: PaintingStationPart.FlowRate, value: 30.0},
-  //     {name: PaintingStationPart.Pressure, value: 60.0},
-  //     {name: PaintingStationPart.ColorConsistency, value: 95.0},
-  //     {name: PaintingStationPart.NozzleCondition, value: 1.0}
-  //   ];
-  //   const expectedHealth = 100;
+  it('calculates PaintingStation NormalRage', () => {
+    const machineName: MachineType = MachineType.PaintingStation;
+    const parts = [
+      {name: PaintingStationPart.FlowRate, value: 30.0},
+      {name: PaintingStationPart.Pressure, value: 60.0},
+      {name: PaintingStationPart.ColorConsistency, value: 95.0},
+      {name: PaintingStationPart.NozzleCondition, value: 1.0}
+    ];
+    const expectedHealth = 100;
 
-  //   const result = calculateMachineHealth(machineName, parts);
-  //   expect(result).toBe(expectedHealth);
-  // });
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
 
-  // it('calculates PaintingStation NormalRage', () => {
-  //   const machineName: MachineType = MachineType.AssemblyLine;
-  //   const parts = [
-  //     {name: AssemblyLinePart.AlignmentAccuracy, value: 1.0},
-  //     {name: AssemblyLinePart.Speed, value: 10.0},
-  //     {name: AssemblyLinePart.FittingTolerance, value: 0.05},
-  //     {name: AssemblyLinePart.BeltSpeed, value: 2.0}
-  //   ];
-  //   const expectedHealth = 100;
+  it('calculates PaintingStation NormalRage', () => {
+    const machineName: MachineType = MachineType.AssemblyLine;
+    const parts = [
+      {name: AssemblyLinePart.AlignmentAccuracy, value: 1.0},
+      {name: AssemblyLinePart.Speed, value: 10.0},
+      {name: AssemblyLinePart.FittingTolerance, value: 0.05},
+      {name: AssemblyLinePart.BeltSpeed, value: 2.0}
+    ];
+    const expectedHealth = 100;
 
-  //   const result = calculateMachineHealth(machineName, parts);
-  //   expect(result).toBe(expectedHealth);
-  // });
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
 
   it('calculates QualityControlStation NormalRage', () => {
     const machineName: MachineType = MachineType.QualityControlStation;
